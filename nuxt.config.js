bca836ac99b0a4e347747b372ba6bf1d21ce6df0
Rahmatputra <rahmatputrankri@gmail.com>
2022-08-24 11:09:27 +0700

    Membuat Proses Login Admin

--- nuxt.config.js
+++ nuxt.config.js
@@ -162,7 +162,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: 'http://localhost:8000'
+    baseURL: 'http://backendecommerce.me/'
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
