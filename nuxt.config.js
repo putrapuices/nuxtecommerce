02b5b0d930effed3cf735cbac8478e23461de044
Rahmatputra <rahmatputrankri@gmail.com>
2022-08-24 10:49:29 +0700

    Installasi CKEDITOR dan Sweet Alert 2

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,10 +64,15 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    { src: '~/plugins/vue-star-rating.js', mode: 'client' },
-    { src: '~/plugins/chart.js', mode: 'client' },
- ],
+  plugins: [{
+      src: '~/plugins/vue-star-rating.js',
+      mode: 'client'
+    },
+    {
+      src: '~/plugins/chart.js',
+      mode: 'client'
+    },
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -83,70 +88,72 @@ export default {
     '@nuxtjs/axios',
     //https://dev.auth.nuxtjs.org/
     '@nuxtjs/auth-next',
+    //https://github.com/avil13/vue-sweetalert2
+    'vue-sweetalert2/nuxt',
   ],
   auth: {
     strategies: {
-  
-        //strategy "admin"
-        admin: {
-          scheme: 'local',
-          token: {
-            property: 'token',
-            required: true,
-            type: 'Bearer'
+
+      //strategy "admin"
+      admin: {
+        scheme: 'local',
+        token: {
+          property: 'token',
+          required: true,
+          type: 'Bearer'
+        },
+        user: {
+          property: 'user',
+          // autoFetch: true
+        },
+        endpoints: {
+          login: {
+            url: '/api/admin/login',
+            method: 'post',
+            propertyName: 'token'
+          },
+          logout: {
+            url: '/api/admin/logout',
+            method: 'post'
           },
           user: {
-            property: 'user',
-            // autoFetch: true
-          },
-          endpoints: {
-            login: {
-              url: '/api/admin/login',
-              method: 'post',
-              propertyName: 'token'
-            },
-            logout: {
-              url: '/api/admin/logout',
-              method: 'post'
-            },
-            user: {
-              url: '/api/admin/user',
-              method: 'get',
-              propertyName: 'user'
-            }
-          },
+            url: '/api/admin/user',
+            method: 'get',
+            propertyName: 'user'
+          }
         },
-  
-        //strategy "customer"
-        customer: {
-          scheme: 'local',
-          token: {
-            property: 'token',
-            required: true,
-            type: 'Bearer'
+      },
+
+      //strategy "customer"
+      customer: {
+        scheme: 'local',
+        token: {
+          property: 'token',
+          required: true,
+          type: 'Bearer'
+        },
+        user: {
+          property: 'user',
+          // autoFetch: true
+        },
+        endpoints: {
+          login: {
+            url: '/api/customer/login',
+            method: 'post',
+            propertyName: 'token'
+          },
+          logout: {
+            url: '/api/customer/logout',
+            method: 'post'
           },
           user: {
-            property: 'user',
-            // autoFetch: true
-          },
-          endpoints: {
-            login: {
-              url: '/api/customer/login',
-              method: 'post',
-              propertyName: 'token'
-            },
-            logout: {
-              url: '/api/customer/logout',
-              method: 'post'
-            },
-            user: {
-              url: '/api/customer/user',
-              method: 'get',
-              propertyName: 'user'
-            }
-          },
+            url: '/api/customer/user',
+            method: 'get',
+            propertyName: 'user'
+          }
         },
-  
+      },
+
     },
   },
 
