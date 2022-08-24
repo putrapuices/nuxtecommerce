9686739a946150bc7bef071273518f99f2900519
Rahmatputra <rahmatputrankri@gmail.com>
2022-08-24 10:27:29 +0700

    Integrasi Dengan CSS dan JavaScript External (Template Dashboard CoreUI)

--- nuxt.config copy.js
+++ nuxt.config copy.js
@@ -1,5 +1,4 @@
 export default {
-
   // Target Deployment
   target: 'server',
 
@@ -8,15 +7,14 @@ export default {
   ssr: true,
 
   loading: {
-    color: 'white',	
-// <-- color
-    height: '5px'	
-// <-- height
+    color: 'white',
+    // <-- color
+    height: '5px'
+    // <-- height
   },
-
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt-ecommerce',
+    title: 'nuxtecommerce',
     htmlAttrs: {
       lang: 'en'
     },
@@ -31,44 +29,30 @@ export default {
         hid: 'description',
         name: 'description',
         content: ''
+      },
+      {
+        name: 'format-detection',
+        content: 'telephone=no'
       }
     ],
     link: [{
-        rel: 'icon',
-        type: 'image/x-icon',
-        href: '/images/logo.png'
-      },
-      {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap'
-      },
-      {
-        rel: 'stylesheet',
-        href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css'
-      },
-    ],
-    script: [
-      { src: '/js/coreui.bundle.min.js' }, 
-      { src: 'https://app.sandbox.midtrans.com/snap/snap.js', 'data-client-key': 'paste_client_Key_midtrans_disini' }, 
-    ]
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '@/assets/css/style.min.css',
-    '@/assets/css/custom.css',
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
@@ -80,10 +64,12 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: 'http://localhost:8000'
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: 'http://backendecommerce.me/'
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {}
+
+
 }
