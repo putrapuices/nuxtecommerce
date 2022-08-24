736138ada445603ece740315f2ca63778f54dec0
Rahmatputra <rahmatputrankri@gmail.com>
2022-08-24 10:43:54 +0700

    Installasi dan Konfigurasi Vue Star Rating

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,7 +64,9 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    { src: '~/plugins/vue-star-rating.js', mode: 'client' },
+ ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
