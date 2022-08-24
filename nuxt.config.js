a156c0257287cdb25bdd8e10c2ddb5a5f2c27585
Rahmatputra <rahmatputrankri@gmail.com>
2022-08-24 10:17:52 +0700

    Konfigurasi SSR dan Target Deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,16 @@
 export default {
+  // Target Deployment
+  target: 'server',
+
+  //rendering mode SSR	
+
+  ssr: true,
+
   loading: {
-    color: 'white',	
-// <-- color
-    height: '5px'	
-// <-- height
+    color: 'white',
+    // <-- color
+    height: '5px'
+    // <-- height
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -11,31 +18,41 @@ export default {
     htmlAttrs: {
       lang: 'en'
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: ''
+      },
+      {
+        name: 'format-detection',
+        content: 'telephone=no'
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
@@ -52,6 +69,7 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {}
+
+
 }
